1fba7d4492c3e0889295e554268cbb010b301a4b
Jeff Abrahamson <jeff@purple.com>
2012-07-19 07:22:03 +0200

    Throw on request to make invalid staging object.

--- src/stage.cpp
+++ src/stage.cpp
@@ -40,6 +40,12 @@ Stage *cryptar::make_stage(StageType in_stage_type, const string &in_base_dir)
 {
         switch(in_stage_type) {
         case stage_invalid:
+                {
+                        // Why do I see an error if this block is not separately scoped?
+                        ostringstream error_message("Unexpected (but known) staging type, ");
+                        error_message << in_stage_type;
+                        throw(runtime_error(error_message.str()));
+                }
         case base_stage:
                 /*
                 {
